b7109d4b87e92e42baf6a5e4f07a0133c87db103
Mayur Samnani <mayursamnani36@gmail.com>
2021-07-08 23:23:46 +0530

    Added 3D pie chart for languages

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-const Pie3D = () => {
-  return <div>chart</div>;
+const ChartComponent = ({ data }) => {
+	const chartConfigs = {
+		type: "pie3d",
+		width: "400",
+		height: "400",
+		dataFormat: "json",
+		dataSource: {
+			chart: {
+				caption: "Languages",
+				theme: "fusion",
+				pieRadius: "35%",
+			},
+			data,
+		},
+	};
+	return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
